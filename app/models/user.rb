99b31b55e711255e08837172820b8290b1ed45c1
ShunTakahashi <tshun.1996@icloud.com>
2019-07-02 07:33:51 +0900

    Add User validates

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,16 @@
 class User < ApplicationRecord
   has_many :blogs
   has_many :favorites, dependent: :destroy
+
+  validates :name,  presence: true, length: { maximum: 30 }
+  before_validation { email.downcase! }
+  validates :email, presence: true, length: { maximum: 255 },
+            format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
+            uniqueness: true
+
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
+
+
+
 end
