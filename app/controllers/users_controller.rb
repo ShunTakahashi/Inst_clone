9730f8066a167cc39bc897f3cba07bfd297c6f14
ShunTakahashi <tshun.1996@icloud.com>
2019-07-04 07:15:00 +0900

    Add user/edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,6 +22,7 @@ class UsersController < ApplicationController
   end
 
   def update
+    @user = User.find(params[:id])
     if @user.update(user_params)
       redirect_to users_path,notice: "編集が完了しました"
     else
