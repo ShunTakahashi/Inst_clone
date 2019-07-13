8a6b9f273690d2840c5af81c82e9ef876658b9ed
ShunTakahashi <tshun.1996@icloud.com>
2019-07-13 16:54:03 +0900

    Add blogs/edit

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,11 +1,15 @@
 class FavoritesController < ApplicationController
   def create
     favorite = current_user.favorites.create(blog_id: params[:blog_id])
-    redirect_to blogs_path, notice: "#{favorite.blog.user.name}さんの投稿にいいね！をしました"
+    redirect_to blogs_path , notice: "#{favorite.blog.user.name}さんの投稿にいいね！をしました"
   end
 
   def destroy
     favorite = current_user.favorites.find_by(id: params[:id]).destroy
-    redirect_to blogs_path, notice: "#{favorite.blog.user.name}さんの投稿のいいね！を取り消しました"
+    redirect_to blogs_path , notice: "#{favorite.blog.user.name}さんの投稿のいいね！を取り消しました"
+  end
+
+  def index
+    @favorites = Favorite.all.order(id: "DESC")
   end
 end
