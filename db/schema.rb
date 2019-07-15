b3a3e698b4809b9419bd2d2c4e03e26a996036ac
ShunTakahashi <tshun.1996@icloud.com>
2019-07-15 14:45:45 +0900

    Add column user_image To users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_03_222954) do
+ActiveRecord::Schema.define(version: 2019_07_15_054314) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 2019_07_03_222954) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "profile"
+    t.text "user_image"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
