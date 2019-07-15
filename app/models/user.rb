2931d7ec8439031a095820a34d8c1ce4161b946f
ShunTakahashi <tshun.1996@icloud.com>
2019-07-15 21:52:53 +0900

    fix validates

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,9 @@ class User < ApplicationRecord
   before_validation { email.downcase! }
   validates :email, presence: true, length: { maximum: 255 },
             format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
-            uniqueness: true
+            uniqueness: true , on: :create
 
   has_secure_password
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :password, presence: true, length: { minimum: 6 }, on: :create
 
 end
