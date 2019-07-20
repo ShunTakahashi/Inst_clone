a45da87d038249753e64dc68b8a8afd96907ed62
ShunTakahashi <tshun.1996@icloud.com>
2019-07-20 11:37:27 +0900

    add

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,7 +83,7 @@ Rails.application.configure do
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
   #
-  config.assets.compile = true
+  config.assets.initialize_on_precompile = false
 
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
