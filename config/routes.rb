4ccd23e5a436b4e05a1359211112bb6d74a02b15
ShunTakahashi <tshun.1996@icloud.com>
2019-07-20 18:32:07 +0900

    fix gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
+
+
   get "/" => "users#index"
   resources :users do
     member do
